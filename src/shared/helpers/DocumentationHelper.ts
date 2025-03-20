27626ec1b6bdb055b78799073e2548aa57ad9fdd
Moiz Naveed <moiz.naveed@iomechs.com>
2025-03-21 01:04:49 +0500

    refactor: update Swagger setup to use custom CSS and JS for improved documentation styling

--- src/shared/helpers/DocumentationHelper.ts
+++ src/shared/helpers/DocumentationHelper.ts
@@ -20,5 +20,12 @@ export function InitializeSwagger(app: INestApplication) {
       return methodKey.replace(/([A-Z])/g, ' $1').trim();
     },
   });
-  SwaggerModule.setup('/api-docs', app, document);
+  // SwaggerModule.setup('/api-docs', app, document);
+  SwaggerModule.setup('api-docs', app, document, {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.0/swagger-ui.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.0/swagger-ui-standalone-preset.js'
+    ]
+  });
 }
