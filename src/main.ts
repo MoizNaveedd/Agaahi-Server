d093a32755cf983f32d1fc860104f63c1b56a406
Moiz Naveed <sporteaze.devs@gmail.com>
2025-05-08 01:59:06 +0500

    fix: Add global validation pipe to enhance request validation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { InitializeSwagger } from './shared/helpers/DocumentationHelper';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Strip out properties not defined in the DTO
+      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are provided
+      transform: true, // Automatically transform payloads to match DTO types
+    }),
+  );
   app.enableCors();
   InitializeSwagger(app);
 
