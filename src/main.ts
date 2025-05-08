c08feafd28a77f75780cc2efeaf907cc05fe8262
Moiz Naveed <sporteaze.devs@gmail.com>
2025-05-08 16:22:05 +0500

    refactor: Comment out global validation pipe configuration in main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true, // Strip out properties not defined in the DTO
-      forbidNonWhitelisted: true, // Throw an error if non-whitelisted properties are provided
-      transform: true, // Automatically transform payloads to match DTO types
-    }),
-  );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true, // Strip out properties not defined in the DTO
+  //     forbidNonWhitelisted: false, // Throw an error if non-whitelisted properties are provided
+  //     transform: true, // Automatically transform payloads to match DTO types
+  //   }),
+  // );
   app.enableCors();
   InitializeSwagger(app);
 
